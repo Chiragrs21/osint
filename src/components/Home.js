8316ad56fa802764b3c0f635de51f3ab19513230
chirag r_s <chiragrs2003@gmail.com>
2024-03-13 18:53:01 +0530

    change4

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,6 +15,13 @@ const Home = () => {
 
                 <div class="alert alert-success" role="alert">
                     <h4 class="alert-heading">Welcome from team Hackquarium</h4>
+                    <p>Open Source Intelligence (OSINT) is a method of gathering information from public or other open sources, which can be used by security experts, national intelligence agencies, or cybercriminals. When used by cyber defenders, the goal is to discover publicly available information related to their organization that could be used by attackers, and take steps to prevent those future attacks.
+                    </p>
+                    <p>OSINT leverages advanced technology to discover and analyze massive amounts of data, obtained by scanning public networks, from publicly available sources like social media networks, and from the deep web—content that is not crawled by search engines, but is still publicly accessible.
+                    </p>
+                    <p>OSINT tools may be open source or proprietary: the distinction should be made between open source code and open source content. Even if the tool itself is not open source, as an OSINT tool, it provides access to openly available content, known as open source intelligence.</p>
+                    <hr />
+                    <p class="mb-0">Follow the guidence as Mentioned below</p>
                     <br />
                 </div>
                 <div class="alert alert-primary" role="alert">
